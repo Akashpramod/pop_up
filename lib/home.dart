319631b7e2599f8eea7cc7bdb8fe0faa5a7cb6fd
akash <akashothayoth@gmail.com>
2024-02-21 13:18:06 +0530

    hi

---
+++ lib/home.dart
@@ -0,0 +1,39 @@
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PopUp', textAlign: TextAlign.center),
+        centerTitle: true,
+        backgroundColor: Colors.yellowAccent,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text('Popup Title'),
+                  content: Text('This is a simple popup message.'),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('Close'),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          child: Text('Show Popup'),
+        ),
+      ),
+    );
+  }
+}
